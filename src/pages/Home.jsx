55a1fc5b3812d1a8d4911f8915057ae7f569bf78
KOZYR001 <Asmorch11122007@yandex.ru>
2026-03-12 08:56:38 +0400

    sort and filter

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,12 +4,18 @@ import { CryptoCard } from "../components/CryptoCard";
 
 export const Home = () => {
     const [cryptoList, setCryptoList] = useState([]);
+    const [filteredList, setFilteredList] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
-    const [viewMode, setViewMode ] = useState("grid");
+    const [viewMode, setViewMode] = useState("grid");
+    const [sortBy, setSortBy] = useState("market_cap_rank");
 
     useEffect(() => {
         fetchCryptoData();
     }, []);
+
+    useEffect(() => {
+        filterAndSort();
+    }, [sortBy, cryptoList])
 
     const fetchCryptoData = async () => {
         try {
@@ -22,13 +28,35 @@ export const Home = () => {
         }
     };
 
+    const filterAndSort = () => {
+        let filtered = [...cryptoList];
+        filtered.sort((a, b) => {
+            switch (sortBy) {
+                case "name":
+                    return a.name.localCompare(b.name);
+                case "price":
+                    return a.current_price - b.current_price;
+                case "price_desc":
+                    return b.current_price - a.current_price;
+                case "change":
+                    return a.price_change_percentage_24h - b.price_change_percentage_24h;
+                case "market_cap":
+                    return a.market_cap - b.market_cap;
+                default:
+                    return a.market_cap_rank - b.market_cap_rank;
+            }
+        });
+
+        setFilteredList(Filtered);
+    }
+
     return (
         <div className="app">
 
             <div className="controls">
                 <div className="filter-group">
-                    <label>SOrt by:</label>
-                    <select>
+                    <label>Sort by:</label>
+                    <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
                         <option value="market_cap_rank">Rank</option>
                         <option value="name">Name</option>
                         <option value="price">Price (Low to High)</option>
